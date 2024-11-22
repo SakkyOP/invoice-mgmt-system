8c89d1227598734e22595a934239bfbf142588bd
SakkyOP <saksham298@gmail.com>
2024-11-22 17:22:27 +0530

    Configuration for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xn)s-mw1fhyq372a4l0^0y664n43_41b@+0p!rn6=e8+4shah+'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-xn)s-mw1fhyq372a4l0^0y664n43_41b@+0p!rn6=e8+4shah+') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -55,7 +56,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True                              # Only for Developement, change to False in production, add Origin url
+CORS_ALLOW_ORIGINS = [
+    os.getenv('FRONTEND_URL', 'http://localhost:5173'),
+]
+
+CORS_ALLOW_ALL_ORIGINS = True if os.getenv('DEV', True) else False
 
 ROOT_URLCONF = 'backend.urls'
 
@@ -121,7 +126,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+# https://docs.djangoproject.com/en/5.1/howto/static-files/\
+    
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = 'static/'
 
